157f76429820810a04921c020e3327ea595debce
patrickm663 <rev0cation2025@outlook.com>
2022-12-02 13:09:50 +0200

    Update day 1 solution

--- day_one/julia/script.jl
+++ day_one/julia/script.jl
@@ -2,16 +2,11 @@ function dayOne(file)
     open(file) do f
         line = 1
         rollingSum = 0
-        maxSum = 0
         elf = 0
         topSums = Int64[]
         while ! eof(f)
             treat = readline(f)
             if isempty(treat)
-                if rollingSum > maxSum
-                    maxSum = rollingSum
-                    elf = line
-                end
                 push!(topSums, rollingSum)
                 rollingSum = 0
                 line += 1
@@ -19,8 +14,10 @@ function dayOne(file)
                 rollingSum += parse(Int, treat)
             end
         end
-        println("$elf : $maxSum")
-        sumTopThree = sum((sort(topSums, rev=true)[1:3]))
+        topSums = sort(topSums, rev=true)
+        topResult = topSums[1]
+        sumTopThree = sum((topSums[1:3]))
+        println("$topResult")
         println("$sumTopThree")
     end
 end
