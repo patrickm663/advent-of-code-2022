603576f8c20843f0517daa5a85068f8fa03ff314
patrickm663 <rev0cation2025@outlook.com>
2022-12-08 23:21:15 +0200

    Add Day 4 Part 2 solution

--- day_four/julia/solution.jl
+++ day_four/julia/solution.jl
@@ -3,14 +3,26 @@ function dayFourPartOne(file)::Int16
     open(file) do f
         while ! eof(f)
             id::String = readline(f)
-            pairValue::Int16 = checkPair(id)
+            pairValue::Int16 = checkOverlap(id)
             sum += pairValue
         end
     end
     return sum
 end
 
-function checkPair(line)::Int16
+function dayFourPartTwo(file)::Int16
+    sum::Int16 = 0
+    open(file) do f
+        while ! eof(f)
+            id::String = readline(f)
+            pairValue::Int16 = checkOverlap(id; all=true)
+            sum += pairValue
+        end
+    end
+    return sum
+end
+
+function checkOverlap(line; all=false)::Int16
     s::Vector = split(line, ",")
     startA::Int16 = parse(Int16, split(s[1], "-")[1])
     endA::Int16 = parse(Int16, split(s[1], "-")[2])
@@ -21,9 +33,14 @@ function checkPair(line)::Int16
         return 1
     elseif (startA ≥ startB && endB ≥ endA)
         return 1
+    elseif all && (endA ≥ startB && startA ≤ endB)
+        #4-8,2-6
+        return 1
     else
         return 0
     end
+
 end
 
 @time @show dayFourPartOne("input.txt")
+@time @show dayFourPartTwo("input.txt")
